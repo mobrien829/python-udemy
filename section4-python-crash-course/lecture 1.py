3a94b1863b926477e52a328ee557cabee1a90f79
Michael <mobrien@colgate.edu>
2020-06-17 14:53:48 -0400

    first lecture finished

---
+++ section4-python-crash-course/lecture 1.py
@@ -0,0 +1,309 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+1
+
+
+# In[2]:
+
+
+1.0
+
+
+# In[3]:
+
+
+1+1
+
+
+# In[4]:
+
+
+1*3
+
+
+# In[5]:
+
+
+1/2
+
+
+# In[6]:
+
+
+2^3
+
+
+# In[7]:
+
+
+2**3
+
+
+# python follows order of operations
+
+# In[8]:
+
+
+2+3*5+5
+
+
+# In[9]:
+
+
+(2+3)*5+5
+
+
+# In[10]:
+
+
+4 % 4
+
+
+# In[11]:
+
+
+4 % 3
+
+
+# modulus ( % ) gives remainder of the division
+
+# In[12]:
+
+
+9 % 2
+
+
+# In[13]:
+
+
+blah = 2
+
+
+# In[14]:
+
+
+blah
+
+
+# In[15]:
+
+
+blah * 2
+
+
+# In[16]:
+
+
+blah = 3
+
+
+# In[17]:
+
+
+blah
+
+
+# In[18]:
+
+
+blah = blah ** blah
+
+
+# In[19]:
+
+
+blah
+
+
+# variables never start with numbers or special characters
+
+# in python we use underscores instead of camelCase
+
+# In[20]:
+
+
+# Strings
+
+
+# In[21]:
+
+
+'single quotes' 
+"double quotes"
+
+
+# In[22]:
+
+
+x
+
+
+# In[23]:
+
+
+x = "hello"
+
+
+# In[24]:
+
+
+x
+
+
+# In[25]:
+
+
+print(x)
+
+
+# In[26]:
+
+
+return print(x)
+
+
+# In[27]:
+
+
+num =12
+name="Sam"
+
+
+# In[28]:
+
+
+'My number is {} and my name is {}'.format(num, name)
+
+
+# In[29]:
+
+
+'My number is {} and my name is {}'.format(name, num)
+
+
+# In[30]:
+
+
+print('My number is {} and my name is {}'.format(num, name))
+
+
+# In[31]:
+
+
+'My number is {num} and my name is {name}'.format(name=num, num=name
+                                                )
+
+
+# In[36]:
+
+
+s = "abcefghijk"
+
+
+# In[37]:
+
+
+s
+
+
+# In[38]:
+
+
+s[0]
+
+
+# In[39]:
+
+
+s[1]
+
+
+# In[40]:
+
+
+s[0:9]
+
+
+# In[41]:
+
+
+s[0:]
+
+
+# In[44]:
+
+
+s[0:3]
+
+
+# In[45]:
+
+
+[1, 2, 3]
+
+
+# lists are python's arrays
+
+# In[46]:
+
+
+my_list=['a', 'b', 'c']
+
+
+# In[48]:
+
+
+my_list[1:3]
+
+
+# In[49]:
+
+
+my_list.append("d")
+
+
+# In[50]:
+
+
+my_list
+
+
+# In[51]:
+
+
+my_list.append("e")
+
+
+# In[52]:
+
+
+print(my_list[0])
+
+
+# In[53]:
+
+
+nest = [1,2,[3,4]]
+
+
+# In[54]:
+
+
+nest[2]
+
+
+# In[55]:
+
+
+nest[2][1]
+
+
+# In[ ]:
+
+
+
+
