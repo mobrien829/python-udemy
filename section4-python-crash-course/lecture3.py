ff1e652be51289829344a8f804fe2464bb3fdfc9
Michael <mobrien@colgate.edu>
2020-06-18 15:05:27 -0400

    moved to lecture 4

--- section4-python-crash-course/lecture3.py
+++ section4-python-crash-course/lecture3.py
@@ -10,4 +10,22 @@ print(out)
 
 # functions
 
+def my_function(name='DefaultName'):
+    print('Hello ' + name)
 
+
+my_function("Michael")
+my_function()
+
+
+def square(num):
+    """
+    THIS IS A DOC STRING THAT CAN GO MULTIPLE LINES LOL YOLO
+    THIS FUNCTION SQUARES A NUMBER
+    """
+    return num**2
+
+
+print(square.__doc__)
+output = square(2)
+print(output)
