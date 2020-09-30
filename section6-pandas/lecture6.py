268b3b6b566515774e1e3f62cc70aa96362987c7
Michael <mobrien@colgate.edu>
2020-09-30 15:21:20 -0400

    finished lecture 6/7

--- section6-pandas/lecture6.py
+++ section6-pandas/lecture6.py
@@ -1,3 +1,12 @@
 # we can use the .groupby() and then a column header string as the argument
 # to make a groupby object that we can perform functions on
 
+# .nunique and .unique: nunique gives number of unique values, .unique gives the actual values
+
+# .value_counts() returns a series of values and number of times they appeared
+
+# we can use .apply(function name) to apply a function to a column
+
+# ex:
+# df['col1'].apply(times2) will double everything in column 2
+
